9399659227fcf692cef3172b8794d09f04a5abc5
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-12-16 16:13:47 +0530

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("original sets: ")
+print(setx)
+print(sety)
+
+setz = setx.intersection(sety)
+print("\nIntersection of the above sets: ")
+print(setz)
